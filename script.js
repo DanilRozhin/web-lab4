b469ec43ff090c1ea9d5d05f6daf44a130fcc31c
Danil <danilrozhin2005@mail.ru>
2025-12-10 00:20:14 +0300

    Added current location styles

--- script.js
+++ script.js
@@ -4,27 +4,7 @@ function getPosition() {
     navigator.geolocation.getCurrentPosition(
         (position) => {
             position_pc = position;
-
-            const lat = position_pc.coords.latitude.toFixed(2);
-            const lon = position_pc.coords.longitude.toFixed(2);
-            const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true`;
-            console.log(url);
-
-            fetch(url)
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error(`HTTP error! status: ${response.status}`);
-                    }
-                    return response.json();
-                })
-                .then(data => {
-                    console.log('Данные от API:', data);
-                    geo_success();
-                })
-                .catch(error => {
-                    console.error('Ошибка при запросе:', error);
-                });
-
+            geoWeather();
         },
 
         (error) => {
@@ -45,6 +25,34 @@ function getPosition() {
             }
         }
     );
+}
+
+function geoWeather() {
+    const lat = position_pc.coords.latitude.toFixed(2);
+    const lon = position_pc.coords.longitude.toFixed(2);
+    const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true`;
+
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Данные от API:', data);
+            const empty = document.querySelector('.empty-state');
+            if (empty) {
+                empty.style.display = 'none';
+            }
+            const curLoc = document.querySelector('.sidebar-list li');
+            if (curLoc && curLoc.textContent.includes('Current location')) {
+                curLoc.classList.add('chosed');
+            }
+        })
+        .catch(error => {
+            console.error('Ошибка получения данных: ', error);
+        });
 }
 
 document.addEventListener('DOMContentLoaded', getPosition);
